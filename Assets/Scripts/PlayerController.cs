ac3e6f89dbbc66353d0f8e14fc9b5772af117c16
alelievr <alelievr@student.42.fr>
2016-11-01 01:56:57 +0100

    begin to implement custom outline mode

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float					speed = 5f;
+	public Vector2					maxSpeed = Vector2.one * 8;
+	public Vector2					spawnOffset =  Vector2.one;
+	public PolygonSpawnPattern		projectileSpawnPattern;
+
+	// Use this for initialization
+	void Start () {
+		projectileSpawnPattern.maxObjects = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		projectileSpawnPattern.direction = transform.up;
+		projectileSpawnPattern.position = transform.position + transform.up * spawnOffset.y;
+		projectileSpawnPattern.InstanciateFramePolygons();
+	}
+
+	void FixedUpdate() {
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+		input.x = Mathf.Clamp(input.x, -maxSpeed.x, maxSpeed.x);
+		input.y = Mathf.Clamp(input.y, -maxSpeed.y, maxSpeed.y);
+
+		transform.position += (Vector3)input * Time.deltaTime * speed;
+
+		Vector3 mouseDiff = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+		float z = Mathf.Atan2(mouseDiff.x, mouseDiff.y) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0, 0, -z);
+	}
+}
--- Assets/Scripts/Polygon.cs
+++ Assets/Scripts/Polygon.cs
@@ -4,13 +4,39 @@ using System.Collections;
 public class Polygon : MonoBehaviour {
 
 	[HideInInspector]
-	public Vector3	direction;
+	public Vector3			direction;
 	[HideInInspector]
-	public float	speed;
+	public AnimationCurve	speedOverLifetime;
+	public Gradient			colorOverLifetime;
+	public float			scale = 1;
+	public float			timeScale = 1;
+
+	new SpriteRenderer		renderer;
+	float					lifetime;
+
+	void OnEnable()
+	{
+		renderer = GetComponent< SpriteRenderer >();
+		lifetime = 0;
+	}
 
 	void OnBecameInvisible()
 	{
+		enabled = false;
 		GameObject.Destroy(gameObject);
+	}
+
+	void Start()
+	{
+		transform.localScale = Vector3.one * scale;
+		Update();
+	}
+
+	void Update()
+	{
+		transform.position += direction * speedOverLifetime.Evaluate(lifetime);
+		renderer.color = colorOverLifetime.Evaluate(lifetime);
+		lifetime += 0.07f * timeScale;
 	}
 
 }
